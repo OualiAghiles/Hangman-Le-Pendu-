e7eda933ff39c8aa7f4479376689793dbc3ae920
ouali <ouali.aghiles@gmail.com>
2018-03-09 03:00:59 +0100

    add modal on lost

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -26,96 +26,100 @@ var toucheErroner = document.querySelector('.lettre-fase')
 var cmptEssais = document.createElement("span")
 cmptEssais.classList.add('cmptEssais')
 // fonction pour generer les _ representant la longueur du mot
-var genererTiret = function () {
-	cmptEssais.innerHTML = nbrEssais
+var genererTiret = function() {
+  cmptEssais.innerHTML = nbrEssais
 
-	for (var i = 0; i < motChoisit.length; i++) {
-		bonneReponse.push('_')
-		// creation de l'element li selon la longueur du mot
-		var li = document.createElement('li');
-		// ajout du text au li
-		li.appendChild(document.createTextNode(' _ '))
-		// ajout du data-index avec valeur i
-		li.setAttribute("data-index", i); // added line
-		// ajout des li au ul
-		entry.appendChild(li)
-		// ajout du ul au DOM
-		underScore.appendChild(entry)
-	}
-	return bonneReponse
+  for (var i = 0; i < motChoisit.length; i++) {
+    bonneReponse.push('_')
+    // creation de l'element li selon la longueur du mot
+    var li = document.createElement('li');
+    // ajout du text au li
+    li.appendChild(document.createTextNode(' _ '))
+    // ajout du data-index avec valeur i
+    li.setAttribute("data-index", i); // added line
+    // ajout des li au ul
+    entry.appendChild(li)
+    // ajout du ul au DOM
+    underScore.appendChild(entry)
+  }
+  return bonneReponse
 }
 
 // creation d'element DOM des mauvaise reponse
-var elementPenduFaulse = function (key) {
-	var span = document.createElement("span") // Create a <li> node
-	var textnode = document.createTextNode(key); // Create a text node
-	span.appendChild(textnode); // Append the text to <li>
-	toucheErroner.appendChild(span); // Append <li> to <ul> with id="myList"
+var elementPenduFaulse = function(key) {
+  var span = document.createElement("span") // Create a <li> node
+  var textnode = document.createTextNode(key); // Create a text node
+  span.appendChild(textnode); // Append the text to <li>
+  toucheErroner.appendChild(span); // Append <li> to <ul> with id="myList"
 }
-// creer les element bonne lettre 
-var guess = function (cmp, touche) {
-	// selectionner le bon element ou ajouter la lettre via le data-index
-	var maLettre = document.querySelector('li[data-index="' + cmp + '"]');
-	maLettre.classList.add('check')
-	// ajouter la lettre a l'element selectionner au prealable
-	maLettre.innerHTML = maLettre.innerHTML.replace(' _ ', touche)
-	return maLettre
+// creer les element bonne lettre
+var guess = function(cmp, touche) {
+  // selectionner le bon element ou ajouter la lettre via le data-index
+  var maLettre = document.querySelector('li[data-index="' + cmp + '"]');
+  maLettre.classList.add('check')
+  // ajouter la lettre a l'element selectionner au prealable
+  maLettre.innerHTML = maLettre.innerHTML.replace(' _ ', touche)
+  return maLettre
 }
 // compteur des essais restant
-var essaisRestant = function (cmptRestant, nbrEssais) {
-	// calcule du nombre d'essais restant
-	var tentative = nbrEssais - (cmptRestant + 1)
-	// affichage du nombre d'essais restant
-	cmptEssais.innerHTML = tentative
-	perdu.appendChild(cmptEssais)
-	
-	
+var essaisRestant = function(cmptRestant, nbrEssais) {
+  // calcule du nombre d'essais restant
+  var tentative = nbrEssais - (cmptRestant + 1)
+  // affichage du nombre d'essais restant
+  cmptEssais.innerHTML = tentative
+  perdu.appendChild(cmptEssais)
+
+
 }
 
 genererTiret()
-document.addEventListener('keypress', function (event) {
-	// Convertion du code de la touche en symbole representant le clavier (lettre , chiffre , etc)
-	var touche = String.fromCharCode(event.charCode).toLowerCase()
-	if (bonneReponse.indexOf(touche) != -1 || misses.indexOf(touche) != -1) {
-		// notifier que la lettre a deja etait utilisé
-		return false
-	}
-	if (motChoisit.indexOf(touche) != -1) {
-		for (var i = 0; i < motChoisit.length; i++) {
-			// comparer la lettre incerer avec l'une des lettre du mot choisit
-			if (motChoisit[i] == touche) {
-				bonneReponse[i] = motChoisit[i]
-				guess(i, touche)
-			}
-		}
-	} else {
-		// condition pour limiter les essais
-		if (essais >= nbrEssais - 1) {
-			// Afficher defaite et demande de rejouer
-			var parts = hangmanParts[essais]
-			parts.classList.add('visible')
-			// si defaite ajouter le texte pour monter le mot non trouver
-			perdu.innerHTML = motChoisi			
-			// demender de rejouer
-			modal('.modal')
-			return false
-		} else {
-			essaisRestant(essais, nbrEssais)
-			// ajout des lettre erroner au tableau misses
-			misses.push(touche)				
-			elementPenduFaulse(touche)
-			// ici code pour les image du pendu			
-			var parts = hangmanParts[essais]
-			parts.classList.add('visible')
-			// incrementation des essais
-			essais++
-		}
-	}
-	// comparer les chaine de caractères pour verifier si le joueur a gagner
-	if (JSON.stringify(bonneReponse) == JSON.stringify(motChoisit)) {
-		// message de felicitation
-		modal('.modal')
-		// demande de rejouer
-		return false
-	}
+document.addEventListener('keypress', function(event) {
+  // Convertion du code de la touche en symbole representant le clavier (lettre , chiffre , etc)
+  var touche = String.fromCharCode(event.charCode).toLowerCase()
+  if (bonneReponse.indexOf(touche) != -1 || misses.indexOf(touche) != -1) {
+    // notifier que la lettre a deja etait utilisé
+    return false
+  }
+  if (motChoisit.indexOf(touche) != -1) {
+    for (var i = 0; i < motChoisit.length; i++) {
+      // comparer la lettre incerer avec l'une des lettre du mot choisit
+      if (motChoisit[i] == touche) {
+        bonneReponse[i] = motChoisit[i]
+        guess(i, touche)
+      }
+    }
+  } else {
+    // condition pour limiter les essais
+    if (essais >= nbrEssais - 1) {
+      // Afficher defaite et demande de rejouer
+      var parts = hangmanParts[essais]
+      parts.classList.add('visible')
+      // si defaite ajouter le texte pour monter le mot non trouver
+      var modalContentBox = document.querySelector('.modal .modal-content .box')
+      var h1 = document.createElement('h1')
+      h1.classList.add('title')
+      var txtnode = document.createTextNode(motChoisi);
+      h1.appendChild(txtnode)
+      modalContentBox.appendChild(h1)
+      showModal()
+      // demender de rejouer
+      return false
+    } else {
+      essaisRestant(essais, nbrEssais)
+      // ajout des lettre erroner au tableau misses)
+      elementPenduFaulse(touche)
+      // ici code pour les image du pendu
+      var parts = hangmanParts[essais]
+      parts.classList.add('visible')
+      // incrementation des essais
+      essais++
+    }
+  }
+  // comparer les chaine de caractères pour verifier si le joueur a gagner
+  if (JSON.stringify(bonneReponse) == JSON.stringify(motChoisit)) {
+    // message de felicitation
+    showModal()
+    // demande de rejouer
+    return false
+  }
 })
